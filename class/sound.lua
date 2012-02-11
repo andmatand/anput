438ad68a3e96e591e911bdd02083e079a47a3fff
Andrew Anderson <andrewloveskitties@gmail.com>
2012-02-10 21:20:30 -0800

    Implemented sound class

---
+++ class/sound.lua
@@ -0,0 +1,25 @@
+Sound = class('Sound')
+
+function Sound:init(file)
+	self.file = file
+	self.sources = {}
+
+	-- Add one source to begin with
+	self:add_source()
+end
+
+function Sound:add_source()
+	table.insert(self.sources, love.audio.newSource(self.file, 'static'))
+end
+
+function Sound:play()
+	for _, s in pairs(self.sources) do
+		if s:isStopped() then
+			love.audio.play(s)
+			return
+		end
+	end
+
+	self:add_source()
+	love.audio.play(self.sources[#self.sources])
+end
