6b192c6bbae74205e2da46fa7366c6efaa59c381
Andrew Anderson <andrewloveskitties@gmail.com>
2012-05-02 21:57:57 -0700

    Was experimenting with non-square pixel aspect ratio

--- conf.lua
+++ conf.lua
@@ -5,6 +5,8 @@ function love.conf(t)
 
 	SCALE_X = 2
 	SCALE_Y = 2
+	--SCALE_Y = SCALE_Y * 1.2 -- Keep authentic 4:3 pixel aspect ratio
+
 	t.screen.width = 320 * SCALE_X
 	t.screen.height = 200 * SCALE_Y
 end
