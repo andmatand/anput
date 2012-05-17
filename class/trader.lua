d790bd31184ba056bd159f327a6cd4b305d36505
Andrew Anderson <andrewloveskitties@gmail.com>
2012-05-16 23:06:51 -0700

    Fixed trader talking bug

--- class/trader.lua
+++ class/trader.lua
@@ -34,7 +34,8 @@ function Trader:find_customer()
     local player = self.room.game.player
 
     -- If the player is touching us
-    if tiles_touching(self.position, player.position) then
+    if (player.room == self.room and
+        tiles_touching(self.position, player.position)) then
         -- Consider him our customer
         self.customer = player
     else
