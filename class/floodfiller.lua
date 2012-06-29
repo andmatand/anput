024036f52d3fd2af16ca7a1f4f2ecb0e45b5722b
Andrew Anderson <andrewloveskitties@gmail.com>
2012-06-28 23:55:32 -0700

    Fixed bug in floodfiller

--- class/floodfiller.lua
+++ class/floodfiller.lua
@@ -7,12 +7,13 @@ FloodFiller = class('FloodFiller')
 function FloodFiller:init(source, hotLava)
     self.source = source -- Needs to have and x and y key, e.g. {x = 2, y = 47}
     self.hotLava = hotLava -- Coordinates which are illegal to traverse
+
+    self.freeTiles = {}
 end
 
 -- Implements a scanline flood-fill algorithm
 function FloodFiller:flood()
     local seeds = {}
-    local freeTiles = {}
 
     -- Add the source position as the first seed
     table.insert(seeds, self.source)
@@ -35,57 +36,47 @@ function FloodFiller:flood()
             -- If this is not the source position
             if x ~= self.source.x or y ~= self.source.y then
                 -- Add this tile to the table of found tiles
-                table.insert(freeTiles, {x = x, y = y})
+                table.insert(self.freeTiles, {x = x, y = y})
             end
 
-            -- If our parent was not on the left side
-            if not seed.parentOnLeft then
-                -- If we are searching the left side and we find a legal
-                -- position
-                if searchLeft and self:legal_position(x - 1, y) then
-                    -- Add a new seed at this position
-                    table.insert(seeds, {x = x - 1, y = y,
-                                         parentOnRight = true})
-                    
-                    -- Stop searching the left side for now
-                    searchLeft = false
+            -- If we are searching the left side and we find a legal
+            -- position
+            if searchLeft and self:legal_position(x - 1, y) then
+                -- Add a new seed at this position
+                table.insert(seeds, {x = x - 1, y = y})
+                
+                -- Stop searching the left side for now
+                searchLeft = false
 
-                -- If we are not searching the left side and we find an illegal
-                -- position
-                elseif (not searchLeft and
-                        not self:legal_position(x - 1, y)) then
-                    -- Start searching the left side again, since the old line
-                    -- would not reach past here
-                    searchLeft = true
-                end
+            -- If we are not searching the left side and we find an illegal
+            -- position
+            elseif (not searchLeft and not self:legal_position(x - 1, y)) then
+                -- Start searching the left side again, since the old line
+                -- would not reach past here
+                searchLeft = true
             end
 
-            -- If our parent was not on the right side
-            if not seed.parentOnRight then
-                -- If we are searching the right side and we find a legal
-                -- position
-                if searchRight and self:legal_position(x + 1, y) then
-                    -- Add a new seed at this position
-                    table.insert(seeds, {x = x + 1, y = y,
-                                         parentOnLeft = true})
-                    
-                    -- Stop searching the right side for now
-                    searchRight = false
-                -- If we are not searching the right side and we find an illegal
-                -- position
-                elseif (not searchRight and
-                        not self:legal_position(x + 1, y)) then
-                    -- Start searching the right side again, since the old line
-                    -- would not reach past here
-                    searchRight = true
-                end
+            -- If we are searching the right side and we find a legal
+            -- position
+            if searchRight and self:legal_position(x + 1, y) then
+                -- Add a new seed at this position
+                table.insert(seeds, {x = x + 1, y = y})
+                
+                -- Stop searching the right side for now
+                searchRight = false
+            -- If we are not searching the right side and we find an illegal
+            -- position
+            elseif (not searchRight and not self:legal_position(x + 1, y)) then
+                -- Start searching the right side again, since the old line
+                -- would not reach past here
+                searchRight = true
             end
 
             y = y + 1
         end
     end
 
-    return freeTiles
+    return self.freeTiles
 end
 
 function FloodFiller:legal_position(x, y)
@@ -100,6 +91,13 @@ function FloodFiller:legal_position(x, y)
             if x == l.x and y == l.y then
                 return false
             end
+        end
+    end
+
+    -- If this tile has already been added to freeTiles
+    for _, t in pairs(self.freeTiles) do
+        if x == t.x and y == t.y then
+            return false
         end
     end
 
