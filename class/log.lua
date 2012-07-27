dd1e001495ad988dd824ea7a54eecc54b0c0b055
Andrew Anderson <andrewloveskitties@gmail.com>
2012-07-26 20:59:33 -0700

    Added hit-logging

--- class/log.lua
+++ class/log.lua
@@ -1,7 +1,12 @@
 Log = class('Log')
 
 function Log:init()
+    self.hits = {}
     self.kills = {}
+end
+
+function Log:add_hit(object)
+    table.insert(self.hits, object)
 end
 
 function Log:add_kill(character)
