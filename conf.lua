7457670ff2a79f4b69a28b463c6af4fb9344e77b
Andrew Anderson <andrewloveskitties@gmail.com>
2012-09-23 19:18:42 -0700

    Changed default graphics scale to 3x

--- conf.lua
+++ conf.lua
@@ -3,8 +3,8 @@ function love.conf(t)
     t.modules.physics = false
     t.version = '0.8.0'
 
-    SCALE_X = 2
-    SCALE_Y = 2
+    SCALE_X = 3
+    SCALE_Y = 3
     --SCALE_Y = SCALE_Y * 1.2 -- Keep authentic 4:3 pixel aspect ratio
 
     t.screen.width = 320 * SCALE_X
